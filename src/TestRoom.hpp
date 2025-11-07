e831f1d0f2ab4151394db5ffd84a4556bd4bb42c
BerlinVincent <vincent.sternberg@gmail.com>
2025-11-08 00:03:26 +0100

    added new TestRoom for a different Tileset

--- src/TestRoom.hpp
+++ src/TestRoom.hpp
@@ -19,4 +19,19 @@ std::vector<std::vector<Tile>> TestRoom() {
     };
 }
 
+std::vector<std::vector<Tile>> TestRoom2() {
+    return {
+        {Tile(4, 4), Tile(4, 5), Tile(4, 5), Tile(4, 5), Tile(4, 5), Tile(4, 5), Tile(4, 5), Tile(4, 5), Tile(4, 5), Tile(4, 6)},
+        {Tile(5, 4), Tile(5, 5), Tile(5, 5), Tile(5, 5), Tile(5, 5), Tile(5, 5), Tile(5, 5), Tile(5, 5), Tile(5, 5), Tile(5, 6)},
+        {Tile(5, 4), Tile(5, 5), Tile(5, 5), Tile(5, 5), Tile(5, 5), Tile(5, 5), Tile(5, 5), Tile(5, 5), Tile(5, 5), Tile(5, 6)},
+        {Tile(5, 4), Tile(5, 5), Tile(5, 5), Tile(5, 5), Tile(5, 5), Tile(5, 5), Tile(5, 5), Tile(5, 5), Tile(5, 5), Tile(5, 6)},
+        {Tile(5, 4), Tile(5, 5), Tile(5, 5), Tile(5, 5), Tile(2, 8), Tile(6, 5), Tile(6, 5), Tile(6, 5), Tile(6, 5), Tile(6, 6)},
+        {Tile(5, 4), Tile(5, 5), Tile(5, 5), Tile(5, 5), Tile(5, 6), Tile(-1, -1), Tile(-1, -1),  Tile(-1, -1),  Tile(-1, -1),  Tile(-1, -1)},
+        {Tile(5, 4), Tile(5, 5), Tile(5, 5), Tile(5, 5), Tile(5, 6), Tile(-1, -1), Tile(4, 4),  Tile(4, 5),  Tile(4, 6),  Tile(-1, -1)},
+        {Tile(5, 4), Tile(5, 5), Tile(5, 5), Tile(5, 5), Tile(9, 13), Tile(7, 5), Tile(6, 5),  Tile(5, 5),  Tile(5, 7),  Tile(-1, -1)},
+        {Tile(5, 4), Tile(5, 5), Tile(5, 5), Tile(5, 5), Tile(5, 6), Tile(-1, -1), Tile(6, 4),  Tile(6, 5),  Tile(6, 6),  Tile(-1, -1)},
+        {Tile(6, 4), Tile(6, 5), Tile(6, 5), Tile(6, 5), Tile(6, 6), Tile(-1, -1), Tile(-1, -1),  Tile(-1, -1),  Tile(-1, -1),  Tile(-1, -1)}
+    };
+}
+
 };
