3c473c4267c975f7f91e4a0ea523cc080e9ea3fe
BerlinVincent <vincent.sternberg@gmail.com>
2025-11-08 13:12:59 +0100

    TestRoom2 is still not completely finished

--- src/TestRoom.hpp
+++ src/TestRoom.hpp
@@ -21,14 +21,14 @@ std::vector<std::vector<Tile>> TestRoom() {
 
 std::vector<std::vector<Tile>> TestRoom2() {
     return {
-        {Tile(4, 4), Tile(4, 5), Tile(4, 5), Tile(4, 5), Tile(4, 5), Tile(4, 5), Tile(4, 5), Tile(4, 5), Tile(4, 5), Tile(4, 6)},
-        {Tile(5, 4), Tile(5, 5), Tile(5, 5), Tile(5, 5), Tile(5, 5), Tile(5, 5), Tile(5, 5), Tile(5, 5), Tile(5, 5), Tile(5, 6)},
-        {Tile(5, 4), Tile(5, 5), Tile(5, 5), Tile(5, 5), Tile(5, 5), Tile(5, 5), Tile(5, 5), Tile(5, 5), Tile(5, 5), Tile(5, 6)},
-        {Tile(5, 4), Tile(5, 5), Tile(5, 5), Tile(5, 5), Tile(5, 5), Tile(5, 5), Tile(5, 5), Tile(5, 5), Tile(5, 5), Tile(5, 6)},
-        {Tile(5, 4), Tile(5, 5), Tile(5, 5), Tile(5, 5), Tile(2, 8), Tile(6, 5), Tile(6, 5), Tile(6, 5), Tile(6, 5), Tile(6, 6)},
+        {Tile(4, 4), Tile(4, 5), Tile(4, 5), Tile(4, 5), Tile(4, 5), Tile(1, 0), Tile(4, 1), Tile(4, 1), Tile(4, 1), Tile(4, 2)},
+        {Tile(5, 4), Tile(5, 5), Tile(5, 5), Tile(5, 5), Tile(5, 5), Tile(1, 0), Tile(1, 1), Tile(1, 1), Tile(1, 1), Tile(5, 2)},
+        {Tile(5, 4), Tile(5, 5), Tile(5, 5), Tile(5, 5), Tile(5, 5), Tile(1, 0), Tile(1, 1), Tile(5, 1), Tile(5, 1), Tile(5, 2)},
+        {Tile(5, 4), Tile(5, 5), Tile(5, 5), Tile(5, 5), Tile(5, 5), Tile(1, 0), Tile(5, 1), Tile(1, 1), Tile(1, 1), Tile(5, 2)},
+        {Tile(5, 4), Tile(5, 5), Tile(5, 5), Tile(5, 5), Tile(2, 8), Tile(1, 0), Tile(6, 1), Tile(6, 1), Tile(6, 1), Tile(6, 2)},
         {Tile(5, 4), Tile(5, 5), Tile(5, 5), Tile(5, 5), Tile(5, 6), Tile(-1, -1), Tile(-1, -1),  Tile(-1, -1),  Tile(-1, -1),  Tile(-1, -1)},
         {Tile(5, 4), Tile(5, 5), Tile(5, 5), Tile(5, 5), Tile(5, 6), Tile(-1, -1), Tile(4, 4),  Tile(4, 5),  Tile(4, 6),  Tile(-1, -1)},
-        {Tile(5, 4), Tile(5, 5), Tile(5, 5), Tile(5, 5), Tile(9, 13), Tile(7, 5), Tile(6, 5),  Tile(5, 5),  Tile(5, 7),  Tile(-1, -1)},
+        {Tile(5, 4), Tile(5, 5), Tile(5, 5), Tile(5, 5), Tile(8, 12), Tile(7, 5), Tile(8, 10),  Tile(5, 5),  Tile(5, 6),  Tile(-1, -1)},
         {Tile(5, 4), Tile(5, 5), Tile(5, 5), Tile(5, 5), Tile(5, 6), Tile(-1, -1), Tile(6, 4),  Tile(6, 5),  Tile(6, 6),  Tile(-1, -1)},
         {Tile(6, 4), Tile(6, 5), Tile(6, 5), Tile(6, 5), Tile(6, 6), Tile(-1, -1), Tile(-1, -1),  Tile(-1, -1),  Tile(-1, -1),  Tile(-1, -1)}
     };
